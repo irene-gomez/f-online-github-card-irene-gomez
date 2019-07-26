ddbf3b55e20df59186b237c5947bcb885dabaeea
irene-gomez <irene@irenegf.com>
2019-07-26 13:31:25 +0200

    Add fetch api github members adalab orgs

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -4,12 +4,32 @@ import './styles.scss';
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      adalabMembers: []
+    };
   }
 
+  componentDidMount() {
+    this.getData();
+  }
+
+  getData() {
+    fetch('https://api.github.com/orgs/Adalab/members')
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          adalabMembers: data
+        });
+        
+      })
+  }
+  
   render() {
     return (
       <div className="App">
-        hola
+        <select name="" id="adalab-adalabUsers">
+
+        </select>
       </div>
     );
   }
