cd6dc3705b871d0f92d7c735da72c723be18516f
irene-gomez <irene@irenegf.com>
2019-07-26 13:37:29 +0200

    Add name member in select

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,37 +2,38 @@ import React from 'react';
 import './styles.scss';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      adalabMembers: []
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            adalabMembers: []
+        };
+    }
 
-  componentDidMount() {
-    this.getData();
-  }
+    componentDidMount() {
+        this.getData();
+    }
 
-  getData() {
-    fetch('https://api.github.com/orgs/Adalab/members')
-      .then(res => res.json())
-      .then(data => {
-        this.setState({
-          adalabMembers: data
-        });
-        
-      })
-  }
-  
-  render() {
-    return (
-      <div className="App">
-        <select name="" id="adalab-adalabUsers">
+    getData() {
+        fetch('https://api.github.com/orgs/Adalab/members')
+            .then(res => res.json())
+            .then(data => {
+                this.setState({
+                    adalabMembers: data
+                });
+            });
+    }
 
-        </select>
-      </div>
-    );
-  }
+    render() {
+        const { adalabMembers } = this.state;
+        return (
+            <div className="App">
+                <select name="" id="adalab-adalabUsers">
+                    <option value="">Selecciona una usuaria</option>
+                    {adalabMembers.map(user => <option value={user.login}>{user.login}</option>)}
+                </select>
+            </div>
+        );
+    }
 }
 
 export default App;
